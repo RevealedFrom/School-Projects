36e332c196c736b056cc03a64ae9bcfeb6c8e4be
blazecollie <szablya@gmail.com>
2013-12-11 18:45:14 -0800

    Create C++Project7.cpp

---
+++ C++Project7.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Car.h"
+#pragma once
+	
+	int year;
+	int speedomiter;
+	string make;
+
+	
+	CarClass startcar( int year, string make);
+	CarClass accelerate( int speedomiter);
+
